3bc3a1e397ef8aecf0a1c37205de317d3babbb4e
纯属@娱乐 <tzrwyx@foxmail.com>
2018-10-20 18:09:21 +0800

    add some files to ikcamp

--- ikcamp/pages/index/index.js
+++ ikcamp/pages/index/index.js
@@ -1,15 +1,17 @@
 //index.js
 //获取应用实例
+import config from '../../utils/config'
+
 const app = getApp()
 
 Page({
   data: {
-    hadLoading: false,
-    pages: 1,
+    hiddenLoading: false,
+    page: 1,
     pageSize: 4,
+    days: 3,
     totalSize: 0,
     hasMore: true,
-    days: 3,
     articleList: [],
     motto: 'Hello World',
     userInfo: {},
@@ -22,17 +24,36 @@ Page({
       url: '../logs/logs'
     })
   },
+  showDetail: function(e) {
+    let dataset = e.currentTarget.dataset;
+    let item = dataset && dataset.item;
+    let contentId = item.contentId || 0;
+    wx.navigateTo({
+      url: `../detail/detail?contentId=${contentId}`
+    });
+  },
   onLoad: function () {
+    let title = config.defaultBarTitle;
+    wx.setNavigationBarTitle({
+      title,
+      success: function(res) {
+        // success
+      }
+    })
     this.requestArticle()
-    },
-    requestArticle: function() {
-      wx.request({
-      url: 'https://www.easy-mock.com/mock/5bca925fe6742c1bf8220bf0/ikcamp/list',
+
+  },
+  requestArticle:function() {
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5bca9240e6742c1bf8220bbd/kicamp/list#!method=get',
       success: (res) => {
         console.log(res.data.data);
         let list = this.data.articleList;
-        list = [...list,...res.data.data];
-        list.push(res.data.data[this.data.page-1]);
+        list = [...list, ...res.data.data];
+        console.log(list);
+        // const list = this.data.articleList;
+        list.push(res.data.data[this.data.page-1])
+        console.log(list)
         this.setData({
           hiddenLoading: true,
           articleList: list
@@ -40,7 +61,6 @@ Page({
       }
     })
   },
-  
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -53,11 +73,10 @@ Page({
     if (this.data.hasMore) {
       let nextPage = this.data.page + 1;
       this.setData({
-        pages: nextPage
+        page: nextPage
       });
       this.requestArticle();
     }
-    console.log('到页面底部了')
-
+    console.log('到页面底部了');
   }
 })

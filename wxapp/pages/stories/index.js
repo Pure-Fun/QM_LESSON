afc35bad3b6eb20bd6d1a0d16bfee59a4ed1b010
纯属@娱乐 <tzrwyx@foxmail.com>
2018-10-23 10:58:32 +0800

    update wxappp

--- wxapp/pages/stories/index.js
+++ wxapp/pages/stories/index.js
@@ -1,11 +1,29 @@
 const app = getApp()
 
 Page({
-    data: {
+  data: {
+    stories: [],
+    currentVid: null,
+    currentVideo: null
+  },
+  // 生命周期
+  onLoad(options) {
+    this.setData({
+      stories: app.globalData.stories
+    })
+  },
+  play(event) {
+    const vid = event.target.dataset.vid
+    // console.log(vid);
+    const currentVideo = wx.createVideoContext(`${vid}`)
+    
+      
 
-    },
-    //生命周期
-    onload(options) {
-        console.log('onload');
-    }
+    this.setData({
+      currentVideo,
+      currentVid: vid
+    }, () => {
+      currentVideo.play()
+    })
+  }
 });

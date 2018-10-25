321a17666c3f73551ab7db8d9f4465cbb5f2f51c
纯属@娱乐 <tzrwyx@foxmail.com>
2018-10-25 19:52:23 +0800

    add npm_hello_script of use stylelint

---
+++ js/npm_hello_script/index.js
@@ -0,0 +1,8 @@
+const str = "不要啊";
+str.charAt(0);
+
+function fn() {
+    // console.log("some beauty");
+}
+
+fn();
---
+++ js/npm_hello_script/readme.md
@@ -0,0 +1,10 @@
+1. 工作流
+  gulp
+   task
+   .src
+   .pipe
+   .dest()
+
+npm 
+  node 包管理器
+  除 webpack 外

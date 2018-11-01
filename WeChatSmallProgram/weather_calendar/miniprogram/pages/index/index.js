3ba02233b99d434214eadb2a97a3ecea64d85943
纯属@娱乐 <tzrwyx@foxmail.com>
2018-11-01 15:47:23 +0800

    add small program of weather_calendar

---
+++ WeChatSmallProgram/weather_calendar/miniprogram/pages/index/index.js
@@ -0,0 +1,76 @@
+//index.js
+const app = getApp();
+const globalData = app.globalData;
+// 凡在根上， globalData 小程序内没有cookie
+Page({
+  data: {
+    auth: -1,
+    nickname: '',
+    avatarUrl: '',
+    emotions: ['serene', 'hehe', 'ecstatic', 'sad', 'terrified'],
+    colors: {
+      'serene': '#64d9fe', 
+      'hehe': '#d3fc1e', 
+      'ecstatic': '#f7dc0e', 
+      'sad': '#ec238a', 
+      'terrified': '#ee1aea'
+    },
+    openid: null,
+    activeEmotion: 'serene'
+  },
+  onLoad() {
+    // 用户授权有很多方面  scope.userInfo
+    this.getScope(this.getUserInfo, () => {
+      this.setData({
+        auth: 0
+      });
+    });
+  },
+  // 高阶函数， success 参数也是一个函数
+  getScope (success, fail, name = 'scope.userInfo') {
+    wx.getSetting({
+      success: (res) => {
+        // console.log(res);
+        if (res.authSetting[name]) {
+          
+          success();
+        } else {
+          fail(); // 箭头函数
+        }
+      }
+    })
+  },
+  getUserInfo () {
+    // console.log('userinfo');
+    if (!globalData.nickname || !globalData.avatarUrl) {
+      this._getUserInfo((res) => {
+        // console.log(res);
+        this.setData({
+          nickname: res.nickName,
+          avatarUrl: res.avatarUrl
+        });
+        globalData.nickname = res.nickName;
+        globalData.avatarUrl = res.avatarUrl
+      });
+    }
+  },
+  _getUserInfo (cb = () => {}) {
+    wx.getUserInfo({
+      success: (res) => {
+        cb(res.userInfo);
+      }
+    })
+  },
+  submitEmotion () {
+    // database 保存一天的记录
+    // 如何在小程序的前端拿到openid?
+    let { openid, activeEmotion } = this.data
+    // addEmotion(openid, activeEmotion);
+  },
+  checkedColor (e) {
+    let activeEmotion = e.currentTarget.dataset.emotion
+    this.setData({
+      activeEmotion
+    })
+  }
+})
---
+++ WeChatSmallProgram/weather_calendar/miniprogram/pages/readme.md
@@ -0,0 +1,10 @@
+- 登录
+  1. _openid
+  2. getUserInfo
+  3. button
+  type="bindauserainfo"
+  auth session cookie
+
+- 高阶函数
+  函数的参数为函数或者返回值为函数， 这个函数就是高阶函数，是高级JS的入口，用户授权与否，及接下来相应的逻辑行为，wx.getSetting() 产生一个分支，相应的事情也是一个函数
+  

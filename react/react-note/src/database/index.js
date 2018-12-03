f0189debc6bd92fed7dbf6e6a10d084c3385bd67
纯属@娱乐 <tzrwyx@foxmail.com>
2018-12-04 00:19:54 +0800

    add react program of react-note

---
+++ react/react-note/src/database/index.js
@@ -0,0 +1,27 @@
+import Loki from 'lokijs';
+import { resolve } from 'url';
+
+export const db = new Loki('notes', {
+  autoload: true,
+  autoloadCallback:
+  databaseInitialize,
+  autosave: true,
+  autosaveInterval: 3000,
+  persistenceMethod: 'localStorage'
+});
+
+function databaseInitialize() {
+  const notes = db.getCollection('notes');
+  if (notes === null) {
+    db.addCollection('notes');
+  }
+}
+
+export function loadCollection(collection) {
+  return new Promise((resolve) => {
+    db.loadDatabase({}, () => {
+      const _collection = db.getCollection(collection);
+      resolve(_collection);
+    })
+  });
+}

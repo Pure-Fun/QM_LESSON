1402b509ed415eef68ce9e33eec757b66f4a0987
纯属@娱乐 <tzrwyx@foxmail.com>
2018-12-21 19:18:57 +0800

    add react program of my-app to react(进度条)

---
+++ react/my-app/src/App.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import './App.css';
+import ProgressBar from './progress';
+
+class App extends React.Component {
+  public state = {
+    color: 'lightblue',
+    showInfo: true,
+    step: 5,
+    total: 12
+  }
+
+  public decline = () => {
+    let step = this.state.step - 1;
+    if (step < 0) {
+      step = this.state.step
+    }
+    this.setState({
+      step
+    })
+  }
+
+  public increase = () => {
+    let step = this.state.step + 1;
+    if (step > this.state.total) {
+      step = this.state.step
+    }
+    this.setState({
+      step
+    })
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <div>
+          <p>
+            <button onClick={this.decline}>-</button>
+            <button onClick={this.increase}>+</button>
+            <label>step:</label>
+            {this.state.step}
+          </p>
+        </div>
+        <ProgressBar
+          step={this.state.step}
+          total={this.state.total}
+          showInfo={this.state.showInfo}
+          color={this.state.color} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/my-app/src/progress.tsx
@@ -0,0 +1,93 @@
+import * as Proptypes from 'prop-types';
+import * as React from 'react';
+import './progress.css';
+
+export interface IProgressProps {
+  color?: string;
+  prefixCls?: string;
+  showInfo?: boolean;
+  step?: number;
+  total?: number;
+}
+
+const percentDeal = (
+  step: number | undefined,
+  total: number | undefined)
+  : number => {
+  if (!step || !total) {
+    return 0;
+  }
+  return (step / total) * 100;
+}
+const parseIntPercent = (text: number): string => `${Math.ceil(text)}%`;
+
+const validProgress = (progress: number | undefined) => {
+  if (!progress || progress < 0) {
+    return 0;
+  } else if (progress > 100) {
+    return 100;
+  }
+  return progress;
+}
+
+class ProgressBar extends React.Component<IProgressProps> {
+  public static defaultProps = {
+    color: '#FFE103',
+    prefixCls: 'tiger-progress',
+    showInfo: false,
+    step: 2,
+    total: 10
+  }
+  public static propTypes = {
+    color: Proptypes.string,
+    showInfo: Proptypes.string,
+    step: Proptypes.number,
+    total: Proptypes.number
+  }
+  public render() {
+    const { prefixCls, step, total, showInfo, color, ...restProps } = this.props;
+    let progress;
+    let text;
+    let percent;
+    let progressInfo;
+    percent = percentDeal(step, total);
+    text = parseIntPercent(validProgress(percent));
+    if (showInfo) {
+      progressInfo = (
+        <div className={`${prefixCls}-show-info`}>
+          <span className={`${prefixCls}-text`}>{text}</span>
+        </div>
+      )
+    }
+
+    const fixOuterStyle = {
+      marginTop: "30px"
+    }
+    const percentStyle = {
+      background: color,
+      borderRadius: "100px",
+      height: "12px",
+      width: text
+    }
+
+    progress = (
+      <div>
+        <div className={`${prefixCls}-outer`} style={fixOuterStyle}>
+          <div className={`${prefixCls}-inner`}>
+            <div className={`${prefixCls}-bg`} style={percentStyle}>
+              {progressInfo || null}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+
+    return (
+      <div {...restProps} className="tiger-progress">
+        {progress}
+      </div>
+    )
+  }
+}
+
+export default ProgressBar;
